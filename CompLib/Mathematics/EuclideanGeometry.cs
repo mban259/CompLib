a9b1f2a5aa95851e07eaa5f80757954b23faa20b
mban259 <mban259@users.noreply.github.com>
2020-03-26 18:52:49 +0900

    Pをクラス内に

--- CompLib/Mathematics/EuclideanGeometry.cs
+++ CompLib/Mathematics/EuclideanGeometry.cs
@@ -6,17 +6,16 @@ namespace CompLib.Mathematics
 
     public class EuclideanGeometry
     {
-    }
-
-    // 2次元ベクトル
-    public struct P
-    {
-        public readonly Num X, Y;
-
-        public P(Num x, Num y)
+        // 2次元ベクトル
+        public struct P
         {
-            X = x;
-            Y = y;
+            public readonly Num X, Y;
+
+            public P(Num x, Num y)
+            {
+                X = x;
+                Y = y;
+            }
         }
     }
 }
