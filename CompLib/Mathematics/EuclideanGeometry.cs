61765315e683cd438fc4f3952d9b339826f3264c
mban259 <mban259@users.noreply.github.com>
2020-03-26 20:43:47 +0900

    struct達をクラス外に

--- CompLib/Mathematics/EuclideanGeometry.cs
+++ CompLib/Mathematics/EuclideanGeometry.cs
@@ -9,13 +9,13 @@ namespace CompLib.Mathematics
     {
         private const Num Eps = (Num) 1e-10;
 
-        static Num Add(Num l, Num r)
+        public static Num Add(Num l, Num r)
         {
             if (Math.Abs(l + r) < Eps * (Math.Abs(l) + Math.Abs(r))) return 0;
             return l + r;
         }
 
-        static bool Equal(Num l, Num r)
+        public static bool Equal(Num l, Num r)
         {
             return Math.Abs(l - r) < Eps * (Math.Abs(l) + Math.Abs(r));
         }
@@ -101,102 +101,102 @@ namespace CompLib.Mathematics
 
             return res;
         }
+    }
 
-        /// <summary>
-        /// 直線 a-b
-        /// </summary>
-        public struct Line
+    /// <summary>
+    /// 直線 a-b
+    /// </summary>
+    public struct Line
+    {
+        public readonly P A, B;
+
+        public Line(P a, P b)
         {
-            public readonly P A, B;
+            A = a;
+            B = b;
+        }
 
-            public Line(P a, P b)
-            {
-                A = a;
-                B = b;
-            }
+        public Line(Num aX, Num aY, Num bX, Num bY)
+        {
+            A = new P(aX, aY);
+            B = new P(bX, bY);
+        }
 
-            public Line(Num aX, Num aY, Num bX, Num bY)
-            {
-                A = new P(aX, aY);
-                B = new P(bX, bY);
-            }
+        public Line(Segment s)
+        {
+            A = s.A;
+            B = s.B;
+        }
+    }
 
-            public Line(Segment s)
-            {
-                A = s.A;
-                B = s.B;
-            }
+    /// <summary>
+    /// 線分 a-b
+    /// </summary>
+    public struct Segment
+    {
+        public readonly P A, B;
+
+        public Segment(P a, P b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public Segment(Num aX, Num aY, Num bX, Num bY)
+        {
+            A = new P(aX, aY);
+            B = new P(bX, bY);
+        }
+
+        public Line ToLine()
+        {
+            return new Line(A, B);
+        }
+    }
+
+    // 2次元ベクトル
+    public struct P
+    {
+        public readonly Num X, Y;
+
+        public P(Num x, Num y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static P operator +(P l, P r)
+        {
+            return new P(EuclideanGeometry.Add(l.X, r.X), EuclideanGeometry.Add(l.Y, r.Y));
+        }
+
+        public static P operator -(P l, P r)
+        {
+            return new P(EuclideanGeometry.Add(l.X, -r.X), EuclideanGeometry.Add(l.Y, -r.Y));
+        }
+
+        public static P operator *(P l, Num r)
+        {
+            return new P(l.X * r, l.Y * r);
         }
 
         /// <summary>
-        /// 線分 a-b
+        /// 内積 l * r = |l|*|r|*cosθ
         /// </summary>
-        public struct Segment
+        /// <returns></returns>
+        public Num Dot(P r)
         {
-            public readonly P A, B;
-
-            public Segment(P a, P b)
-            {
-                A = a;
-                B = b;
-            }
-
-            public Segment(Num aX, Num aY, Num bX, Num bY)
-            {
-                A = new P(aX, aY);
-                B = new P(bX, bY);
-            }
-
-            public Line ToLine()
-            {
-                return new Line(A, B);
-            }
+            return EuclideanGeometry.Add(X * r.X, Y * r.Y);
         }
 
-        // 2次元ベクトル
-        public struct P
+        /// <summary>
+        /// 外積 l * r = |l|*|r|*sinθ
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public Num Det(P r)
         {
-            public readonly Num X, Y;
-
-            public P(Num x, Num y)
-            {
-                X = x;
-                Y = y;
-            }
-
-            public static P operator +(P l, P r)
-            {
-                return new P(Add(l.X, r.X), Add(l.Y, r.Y));
-            }
-
-            public static P operator -(P l, P r)
-            {
-                return new P(Add(l.X, -r.X), Add(l.Y, -r.Y));
-            }
-
-            public static P operator *(P l, Num r)
-            {
-                return new P(l.X * r, l.Y * r);
-            }
-
-            /// <summary>
-            /// 内積 l * r = |l|*|r|*cosθ
-            /// </summary>
-            /// <returns></returns>
-            public Num Dot(P r)
-            {
-                return Add(X * r.X, Y * r.Y);
-            }
-
-            /// <summary>
-            /// 外積 l * r = |l|*|r|*sinθ
-            /// </summary>
-            /// <param name="r"></param>
-            /// <returns></returns>
-            public Num Det(P r)
-            {
-                return Add(X * r.Y, -Y * r.X);
-            }
+            return EuclideanGeometry.Add(X * r.Y, -Y * r.X);
         }
     }
 }
--- UnitTest/Mathematics/EuclideanGeometryTest.cs
+++ UnitTest/Mathematics/EuclideanGeometryTest.cs
@@ -11,10 +11,10 @@ namespace UnitTest.Mathematics
         [TestMethod]
         public void AGC021BSample1()
         {
-            var ps = new EuclideanGeometry.P[2];
-            ps[0] = new EuclideanGeometry.P(0, 0);
-            ps[1] = new EuclideanGeometry.P(1, 1);
-            List<EuclideanGeometry.P> l = new List<EuclideanGeometry.P>();
+            var ps = new P[2];
+            ps[0] = new P(0, 0);
+            ps[1] = new P(1, 1);
+            List<P> l = new List<P>();
             var r = EuclideanGeometry.ConvexHull(ps, l);
             Assert.AreEqual(2, r.Length);
             Assert.AreEqual(2, l.Count);
@@ -25,13 +25,13 @@ namespace UnitTest.Mathematics
         [TestMethod]
         public void AGC021BSample2()
         {
-            var ps = new EuclideanGeometry.P[5];
-            ps[0] = new EuclideanGeometry.P(0, 0);
-            ps[1] = new EuclideanGeometry.P(2, 8);
-            ps[2] = new EuclideanGeometry.P(4, 5);
-            ps[3] = new EuclideanGeometry.P(2, 6);
-            ps[4] = new EuclideanGeometry.P(3, 10);
-            List<EuclideanGeometry.P> l = new List<EuclideanGeometry.P>();
+            var ps = new P[5];
+            ps[0] = new P(0, 0);
+            ps[1] = new P(2, 8);
+            ps[2] = new P(4, 5);
+            ps[3] = new P(2, 6);
+            ps[4] = new P(3, 10);
+            List<P> l = new List<P>();
             var r = EuclideanGeometry.ConvexHull(ps, l);
             Assert.AreEqual(4, r.Length);
             Assert.AreEqual(4, l.Count);
