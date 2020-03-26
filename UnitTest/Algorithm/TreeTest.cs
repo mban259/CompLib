366a2d43b5c26eec4c4b2ff59355fee6dd3cc6e8
mban259 <mban259@users.noreply.github.com>
2020-03-27 03:59:58 +0900

    Initその他削除 テストは後で整備する

--- UnitTest/Algorithm/TreeTest.cs
+++ UnitTest/Algorithm/TreeTest.cs
@@ -8,40 +8,6 @@ namespace UnitTest.Algorithm
     [TestClass]
     public class TreeTest
     {
-        private const int MaxTreeSize = 200000;
-
-        private List<int>[] RandomTreeEdge, LineTreeEdge;
-
-        [ClassInitialize]
-        public void Init()
-        {
-            var rnd = new Random();
-            int rtSize = rnd.Next(MaxTreeSize / 2, MaxTreeSize);
-            int ltSize = rnd.Next(MaxTreeSize / 2, MaxTreeSize);
-            var rt = new MakeTree(rtSize);
-            var lt = new MakeTree(ltSize);
-            for (int i = 0; i < rtSize - 1; i++)
-            {
-                while (true)
-                {
-                    int a = rnd.Next(rtSize);
-                    int b = rnd.Next(rtSize);
-                    if (rt.Connect(a, b))
-                    {
-                        break;
-                    }
-                }
-            }
-
-            for (int i = 0; i < ltSize - 1; i++)
-            {
-                lt.Connect(i, i + 1);
-            }
-
-            RandomTreeEdge = rt.ToEdges();
-            LineTreeEdge = lt.ToEdges();
-        }
-
         [TestMethod]
         public void Test1()
         {
@@ -79,78 +45,6 @@ namespace UnitTest.Algorithm
             Assert.AreEqual(3, tree.Depth(4));
 
             Assert.AreEqual(1, tree.Ancestor(3, 2));
-        }
-
-        [TestMethod]
-        public void RandomTest()
-        {
-        }
-    }
-
-    class MakeTree
-    {
-        private readonly int[] _parent;
-        private readonly int[] _rank;
-        private readonly List<int>[] _edge;
-
-        public MakeTree(int size)
-        {
-            _parent = new int[size];
-            _rank = new int[size];
-            for (int i = 0; i < size; i++)
-            {
-                _parent[i] = i;
-            }
-
-            _edge = new List<int>[size];
-            for (int i = 0; i < size; i++)
-            {
-                _edge[i] = new List<int>();
-            }
-        }
-
-        public bool Connect(int a, int b)
-        {
-            a = Root(a);
-            b = Root(b);
-            if (a == b) return false;
-
-            if (_rank[a] < _rank[b])
-            {
-                _parent[a] = b;
-            }
-            else
-            {
-                _parent[b] = a;
-                if (_rank[a] == _rank[b])
-                {
-                    _rank[a]++;
-                }
-            }
-
-            _edge[a].Add(b);
-            _edge[b].Add(a);
-            return true;
-        }
-
-        public bool Connected(int a, int b)
-        {
-            return Root(a) == Root(b);
-        }
-
-        private int Root(int i)
-        {
-            if (_parent[i] == i)
-            {
-                return i;
-            }
-
-            return Root(_parent[i]);
-        }
-
-        public List<int>[] ToEdges()
-        {
-            return _edge;
         }
     }
 }
