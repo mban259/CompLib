595102bfaec5ef4ab88d36aa6d417f7cede51712
mban259 <mban259@users.noreply.github.com>
2020-04-15 17:04:15 +0900

    追加: BIT

---
+++ CompLib/Collections/FenwickTree.cs
@@ -0,0 +1,49 @@
+namespace CompLib.Collections
+{
+    using Num = System.Int32;
+
+    public class FenwickTree
+    {
+        private readonly Num[] _array;
+        public readonly int Count;
+
+        public FenwickTree(int size)
+        {
+            _array = new Num[size + 1];
+            Count = size;
+        }
+
+        /// <summary>
+        /// A[i]にnを加算
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="n"></param>
+        public void Add(int i, Num n)
+        {
+            i++;
+            for (; i <= Count; i += i & -i)
+            {
+                _array[i] += n;
+            }
+        }
+
+        /// <summary>
+        /// [0,r)の和を求める
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public Num Sum(int r)
+        {
+            Num result = 0;
+            for (; r > 0; r -= r & -r)
+            {
+                result += _array[r];
+            }
+
+            return result;
+        }
+
+        // [l,r)の和を求める
+        public Num Sum(int l, int r) => Sum(r) - Sum(l);
+    }
+}
