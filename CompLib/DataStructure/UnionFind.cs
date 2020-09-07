f55ddd77bcd1013c7d135630f72c0ed14ba992df
mban <mban259@users.noreply.github.com>
2020-09-08 01:50:17 +0900

    UnionFind追加

---
+++ CompLib/DataStructure/UnionFind.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CompLib.DataStructure
+{
+    class UnionFind
+    {
+        private readonly int _n;
+        private readonly int[] _parent, _size;
+
+        /// <summary>
+        /// n頂点の無向グラフに 1.辺を追加, 2.2頂点が同じ連結成分に属するか判定 ができるデータ構造
+        /// </summary>
+        /// <param name="n">頂点の個数</param>
+        public UnionFind(int n)
+        {
+            _n = n;
+            _parent = new int[_n];
+            _size = new int[_n];
+            for (int i = 0; i < _n; i++)
+            {
+                _parent[i] = i;
+                _size[i] = 1;
+            }
+        }
+
+        /// <summary>
+        /// iがいる連結成分の代表値
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public int Find(int i) => _parent[i] == i ? i : Find(_parent[i]);
+
+        /// <summary>
+        /// x,yが同じ連結成分にいるか?
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool Same(int x, int y) => Find(x) == Find(y);
+
+        /// <summary>
+        /// (x, y)に辺を追加する x,yが既に同じ成分ならfalseを返す
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool Connect(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+            if (x == y) return false;
+
+            // データ構造をマージする一般的なテク
+            if (_size[x] > _size[y])
+            {
+                _parent[y] = x;
+                _size[x] += _size[y];
+            }
+            else
+            {
+                _parent[x] = y;
+                _size[y] += _size[x];
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// iが含まれる成分のサイズ
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public int GetSize(int i) => _size[Find(i)];
+
+        /// <summary>
+        /// 連結成分のリスト
+        /// </summary>
+        /// <returns></returns>
+        List<int>[] Groups()
+        {
+            var leaderBuf = new int[_n];
+            var groupSize = new int[_n];
+            for (int i = 0; i < _n; i++)
+            {
+                leaderBuf[i] = Find(i);
+                groupSize[leaderBuf[i]]++;
+            }
+
+            var result = new List<int>[_n];
+            for (int i = 0; i < _n; i++)
+            {
+                result[i] = new List<int>(groupSize[i]);
+            }
+
+            for (int i = 0; i < _n; i++)
+            {
+                result[leaderBuf[i]].Add(i);
+            }
+
+            return result.Where(ls => ls.Count > 0).ToArray();
+        }
+    }
+}
