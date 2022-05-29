5f6595cfc4c52d13dcd39a2fa27ad4696d90894f
mban259 <mban259@users.noreply.github.com>
2022-05-29 17:23:03 +0900

    追加 deque

---
+++ CompLib/Collections/Generic/Deque.cs
@@ -0,0 +1,168 @@
+﻿namespace CompLib.Collections.Generic
+{
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class Deque<T>
+    {
+        private int _offset, _mask;
+        private T[] _buffer;
+        public Deque() : this(16) { }
+
+        public Deque(int capacity)
+        {
+            Debug.Assert(capacity > 0);
+            Debug.Assert((capacity & (capacity - 1)) == 0);
+            _mask = capacity - 1;
+            _offset = 0;
+            _buffer = new T[capacity];
+            Count = 0;
+        }
+
+        /// <summary>
+        /// 末尾に要素追加 O(1)
+        /// </summary>
+        /// <param name="item"></param>
+        public void PushBack(T item)
+        {
+            if (Count == _buffer.Length) Extend();
+            _buffer[(_offset + Count++) & _mask] = item;
+        }
+
+        /// <summary>
+        /// 末尾の要素を削除し、返す O(1)
+        /// </summary>
+        /// <returns></returns>
+        public T PopBack()
+        {
+            Debug.Assert(Count > 0);
+            return _buffer[(_offset + --Count) & _mask];
+        }
+
+        /// <summary>
+        /// 先頭に要素追加 O(1)
+        /// </summary>
+        /// <param name="item"></param>
+        public void PushFront(T item)
+        {
+            if (Count == _buffer.Length) Extend();
+            Count++;
+            _buffer[(--_offset) & _mask] = item;
+        }
+
+        /// <summary>
+        /// 先頭の要素を削除し、返す O(1)
+        /// </summary>
+        /// <returns></returns>
+        public T PopFront()
+        {
+            Debug.Assert(Count > 0);
+            Count--;
+            return _buffer[(_offset++) & _mask];
+        }
+
+        private void Extend()
+        {
+            int capacity = _buffer.Length;
+            Debug.Assert(Count == capacity);
+            T[] tmpBuf = new T[2 * capacity];
+            for (int i = 0; i < capacity; i++)
+            {
+                tmpBuf[i] = _buffer[(_offset + i) & _mask];
+            }
+            _mask = 2 * capacity - 1;
+            _buffer = tmpBuf;
+            _offset = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public T First
+        {
+            get
+            {
+                Debug.Assert(Count > 0);
+                return _buffer[_offset & _mask];
+            }
+        }
+        public T Last
+        {
+            get
+            {
+                Debug.Assert(Count > 0);
+                return _buffer[(_offset + Count - 1) & _mask];
+            }
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                Debug.Assert(0 <= i && i < Count);
+                return _buffer[(_offset + i) & _mask];
+            }
+            set
+            {
+                Debug.Assert(0 <= i && i < Count);
+                _buffer[(_offset + i) & _mask] = value;
+            }
+        }
+
+        public void Clear() { Count = 0; }
+
+        public void Insert(int index, T item)
+        {
+            Debug.Assert(0 <= index && index <= Count);
+            if (Count == _buffer.Length) Extend();
+            if (Count - index - 1 <= index)
+            {
+                for (int i = Count - 1; i >= index; i--)
+                {
+                    _buffer[(_offset + i + 1) & _mask] = _buffer[(_offset + i) & _mask];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    _buffer[(_offset + i - 1) & _mask] = _buffer[(_offset + i) & _mask];
+                }
+                _offset--;
+            }
+            _buffer[(_offset + index) & _mask] = item;
+            Count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Debug.Assert(0 <= index && index < Count);
+
+            if (Count - index - 1 <= index)
+            {
+                for (int i = index + 1; i < Count; i++)
+                {
+                    _buffer[(_offset + i - 1) & _mask] = _buffer[(_offset + i) & _mask];
+                }
+            }
+            else
+            {
+                for (int i = index - 1; i >= 0; i--)
+                {
+                    _buffer[(_offset + i + 1) & _mask] = _buffer[(_offset + i) & _mask];
+                }
+                _offset++;
+            }
+            Count--;
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                array[i] = _buffer[(_offset + i) & _mask];
+            }
+            return array;
+        }
+    }
+}
---
+++ UnitTest/Collections/Generic/DequeTest.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CompLib.Collections.Generic;
+using System.Diagnostics;
+
+namespace UnitTest.Collections.Generic
+{
+    [TestClass]
+    public class DequeTest
+    {
+        [TestMethod]
+        public void TestBack()
+        {
+            var deq = new Deque<int>();
+            const int len = 100000;
+            var rnd = new Random();
+            int[] array = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                int r = rnd.Next();
+                array[i] = r;
+                deq.PushBack(r);
+            }
+
+
+            Assert.AreEqual(len, deq.Count);
+
+            for (int i = 0; i < len; i++)
+            {
+                Assert.AreEqual(array[i], deq[i]);
+            }
+
+            for (int i = len - 1; i >= 0; i--)
+            {
+                Assert.AreEqual(array[i], deq.PopBack());
+            }
+
+            Assert.AreEqual(0, deq.Count);
+
+        }
+
+        [TestMethod]
+        public void TestFront()
+        {
+            var deq = new Deque<int>();
+            const int len = 100000;
+            var rnd = new Random();
+            int[] array = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                int r = rnd.Next();
+                array[i] = r;
+                deq.PushFront(r);
+            }
+
+
+            Assert.AreEqual(len, deq.Count);
+
+            for (int i = 0; i < len; i++)
+            {
+                Assert.AreEqual(array[len - i - 1], deq[i]);
+            }
+
+            for (int i = len - 1; i >= 0; i--)
+            {
+                Assert.AreEqual(array[i], deq.PopFront());
+            }
+
+            Assert.AreEqual(0, deq.Count);
+        }
+
+        [TestMethod]
+        public void TestFB()
+        {
+            var deq = new Deque<int>();
+            var ls = new LinkedList<int>();
+            const int len = 100000;
+            var rnd = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                int r = rnd.Next();
+                if (rnd.Next() % 2 == 0)
+                {
+                    deq.PushFront(r);
+                    ls.AddFirst(r);
+                }
+                else
+                {
+                    deq.PushBack(r);
+                    ls.AddLast(r);
+                }
+            }
+
+            Assert.AreEqual(len, deq.Count);
+
+            for (int i = 0; i < len; i++)
+            {
+                Assert.AreEqual(ls.First.Value, deq.First);
+                Assert.AreEqual(ls.Last.Value, deq.Last);
+                if (rnd.Next() % 2 == 0)
+                {
+                    Assert.AreEqual(ls.First.Value, deq.PopFront());
+                    ls.RemoveFirst();
+                }
+                else
+                {
+                    Assert.AreEqual(ls.Last.Value, deq.PopBack());
+                    ls.RemoveLast();
+                }
+            }
+
+            Assert.AreEqual(0, deq.Count);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            int len = 1000;
+            var ls = new List<int>();
+            var deq = new Deque<int>();
+            var rnd = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                int index = rnd.Next(0, ls.Count + 1);
+                int r = rnd.Next();
+                deq.Insert(index, r);
+                ls.Insert(index, r);
+            }
+
+            Assert.AreEqual(len, deq.Count);
+
+            for (int i = 0; i < len; i++)
+            {
+                Assert.AreEqual(ls[i], deq[i]);
+            }
+        }
+
+        [TestMethod]
+        public void RemoveAtTest()
+        {
+            int len = 1000;
+            var ls = new List<int>();
+            var deq = new Deque<int>();
+            var rnd = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                int r = rnd.Next();
+                if (rnd.Next() % 2 == 0)
+                {
+                    deq.PushFront(r);
+                    ls.Insert(0, r);
+                }
+                else
+                {
+                    deq.PushBack(r);
+                    ls.Add(r);
+                }
+            }
+
+            Assert.AreEqual(len, deq.Count);
+
+            for (int i = len; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Assert.AreEqual(ls[j], deq[j]);
+                }
+
+                int index = rnd.Next(deq.Count);
+                ls.RemoveAt(index);
+                deq.RemoveAt(index);
+            }
+
+            Assert.AreEqual(0, deq.Count);
+        }
+
+
+    }
+}
